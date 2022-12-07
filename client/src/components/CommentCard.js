18858a3c581b665da7aa49ecc9f32cab5baf5681
darrenwin321 <darren.nguyen@stonybrook.edu>
2022-12-07 04:30:18 -0500

    stylish
    minor adjustments to comment cards

--- client/src/components/CommentCard.js
+++ client/src/components/CommentCard.js
@@ -8,13 +8,14 @@ function CommentCard(props){
     let card = 
     <Box 
         sx={{bgcolor: '#8000F00F', borderRadius:"30px", borderTopLeftRadius:'27px', my:'9px'}}
-        height= '120px'
         width='93%'
     >
-        <Typography sx={{transform:"translate(7%,10%)", fontFamily:"Lexend Exa", fontSize:'18px'}}>
+        <Typography sx={{transform:"translate(7%,10%)", fontFamily:"Lexend Exa", fontSize:'18px', fontWeight: 'bold'}}>
             {userText}
         </Typography>
-        <Typography sx={{transform:"translate(8%,5%)", fontFamily:"Lexend Exa", fontSize:'18px'}} width='20vw'>
+        <Typography sx={{transform:"translate(8%,0%)", fontFamily:"Lexend Exa", fontSize:'18px'}} width='20vw' 
+        style={{wordWrap: 'break-word'}}
+        >
             {comment}
         </Typography>
     </Box>
